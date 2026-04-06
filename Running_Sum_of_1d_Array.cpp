83d81272a00096cea28daa2ae49d986f0b5fe7e7
9nency-debug <9.nency@gmail.com>
2026-04-07 00:11:24 +0530

    refactor(prefix-sum): optimize running sum using mathematical recurrence relation (O(n))

--- Running_Sum_of_1d_Array.cpp
+++ Running_Sum_of_1d_Array.cpp
@@ -1,20 +1,45 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// class Solution {
+// public:
+//     vector<int> runningSum(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans(n);
+//         for(int i = 0; i < n; i++) {
+//             int sum = 0;
+//             for(int j = 0; j <= i; j++) {
+//                 sum += nums[j];
+//             }
+//             ans[i] = sum;
+//         }
+        
+//         return ans;
+//     }
+// };
+// int main() {
+//     Solution sol;
+//     vector<int> nums = {1, 2, 3, 4};
+//     vector<int> result = sol.runningSum(nums);
+//     for(int num : result) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
-#include <vector>
+#include <cmath>
 using namespace std;
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
-        for(int i = 0; i < n; i++) {
-            int sum = 0;
-            for(int j = 0; j <= i; j++) {
-                sum += nums[j];
-            }
-            ans[i] = sum;
+        for(int i = 1;i < nums.size(); i++) {
+            nums[i] = nums[i] + nums[i-1];
         }
-        
-        return ans;
+
+        return nums;
     }
 };
 int main() {
