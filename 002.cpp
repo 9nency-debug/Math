1923eff305af0d39ed8005a0347b6aaefa8f108e
9nency-debug <9.nency@gmail.com>
2026-04-10 23:55:54 +0530

    feat: added

---
+++ 002.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+
+        for (int i = 0; i < numRows; i++) {
+            vector<int> row(i + 1, 1); 
+
+            for (int j = 1; j < i; j++) {
+                row[j] = result[i - 1][j - 1] + result[i - 1][j];
+            }
+
+            result.push_back(row);
+        }
+
+        return result;
+    }
+};
+int main() {
+    Solution solution;
+    int numRows = 5; 
+    vector<vector<int>> pascalTriangle = solution.generate(numRows);
+
+    for (const auto& row : pascalTriangle) {
+        for (int num : row) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
